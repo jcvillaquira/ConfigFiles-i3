eaa5d3b3ca93f7d613e2bc11141ffae56de7c13a
jcvillaquira <julian.villaquira@quantil.com.co>
2022-06-03 23:11:26 -0500

    Added VS config files.

--- startup.sh
+++ startup.sh
@@ -6,7 +6,8 @@ cp ecopetrol.sh ~/JFiles/ConfigFiles-i3/
 cp smurfit.sh ~/JFiles/ConfigFiles-i3/
 
 cp ~/.i3/config ~/JFiles/ConfigFiles-i3/
-
 cp ~/.config/nvim/init.vim ~/JFiles/ConfigFiles-i3/
+cp ~/.config/Code/User/settings.json ~/JFiles/ConfigFiles-i3/
+cp ~/.config/Code/User/keybindings.json ~/JFiles/ConfigFiles-i3/
 
 chromium & disown
