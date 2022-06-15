6b97c8c8c09519294c2100611d3362c9f1346a0e
jcvillaquira <julian.villaquira@quantil.com.co>
2022-06-14 19:29:35 -0500

    Change color scheme.

--- startup.sh
+++ startup.sh
@@ -9,5 +9,6 @@ cp ~/.i3/config ~/JFiles/ConfigFiles-i3/
 cp ~/.config/nvim/init.vim ~/JFiles/ConfigFiles-i3/
 cp ~/.config/Code/User/settings.json ~/JFiles/ConfigFiles-i3/
 cp ~/.config/Code/User/keybindings.json ~/JFiles/ConfigFiles-i3/
+cp ~/.config/alacritty/alacritty.yml ~/JFiles/ConfigFiles-i3/
 
 chromium & disown
